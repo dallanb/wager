38c5d704dc6858f64d5ba72fa6b6087b9531e23d
Dallan Bhatti <dallan.bhatti@techtapir.com>
2020-08-04 16:32:04 -0700

    prepare dev to follow similar pattern of test

--- build/bin/entrypoint.sh
+++ build/bin/entrypoint.sh
@@ -4,5 +4,24 @@
 
 pip install -e .
 
+if [ "$DATABASE" = "wager" ]; then
+  echo "Waiting for wager..."
+
+  while ! nc -z $SQL_HOST $SQL_PORT; do
+    sleep 0.1
+  done
+
+  echo "PostgreSQL started"
+fi
+
+if [ ! -d "migrations/dev/versions" ]; then
+  echo "Directory migrations/dev/versions does not exist."
+  flask db init --directory=migrations/dev
+  sed -i '/import sqlalchemy as sa/a import sqlalchemy_utils' migrations/dev/script.py.mako
+  flask db migrate --directory=migrations/dev
+fi
+
+flask db upgrade --directory=migrations/dev
+
 
 manage run -h 0.0.0.0
