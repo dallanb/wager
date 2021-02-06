6086206bfd3d65b09d159f404fd5cc46c17526a3
Dallan Bhatti <dallan.bhatti@techtapir.com>
2021-02-06 11:31:16 -0800

    clean up the test and also re add tests back to dockerfile qaw

--- bin/test.sh
+++ bin/test.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-. ~/.bashrc
-python manage.py delete
-python manage.py create
-python manage.py load
-python -m py.test --disable-pytest-warnings -s
+docker exec wager python manage.py create
+docker exec wager python manage.py load
+docker exec wager py.test --disable-pytest-warnings -s
