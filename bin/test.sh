22aff9de6560775f222f636fa7d5f6378864b51b
Dallan Bhatti <dallan.bhatti@techtapir.com>
2021-02-06 12:25:24 -0800

    make sure wager is healthy and running before running the tests

--- bin/test.sh
+++ bin/test.sh
@@ -2,10 +2,10 @@
 
 echo "Waiting for server..."
 
-while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' 0.0.0.0:80/ping)" != "200" ]]; do sleep 1; done
+while [[ ! "$(docker ps -aq -f status=running -f health=healthy -f name=wager)" ]]; do sleep 1; done
 
 echo "Server launched"
 
-#docker exec wager python manage.py create
-#docker exec wager python manage.py load
-#docker exec wager py.test --disable-pytest-warnings -s
+docker exec wager python manage.py create
+docker exec wager python manage.py load
+docker exec wager py.test --disable-pytest-warnings -s
