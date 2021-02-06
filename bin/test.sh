5a7c94f7cb644df93c640e1cd93a17cb78a37732
Dallan Bhatti <dallan.bhatti@techtapir.com>
2021-02-06 12:05:46 -0800

    update test sh to wait for the server to be up and ready for requests

--- bin/test.sh
+++ bin/test.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-docker exec wager python manage.py create
-docker exec wager python manage.py load
-docker exec wager py.test --disable-pytest-warnings -s
+echo "Waiting for server..."
+
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' 0.0.0.0:80/ping)" != "200" ]]; do sleep 1; done
+
+echo "Server launched"
+
+#docker exec wager python manage.py create
+#docker exec wager python manage.py load
+#docker exec wager py.test --disable-pytest-warnings -s
