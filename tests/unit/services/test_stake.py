c69fd02d68d0e3334228fc299b91590a6d2572da
Dallan Bhatti <dallan.bhatti@techtapir.com>
2021-02-18 18:16:22 -0800

    partially updtes services test on stakes

--- tests/unit/services/test_stake.py
+++ tests/unit/services/test_stake.py
@@ -1,8 +1,8 @@
+import pytest
+
 from src import services, ManualException
 from tests.helpers import generate_uuid
 
-global_stake = None
-global_participant = None
 stake_service = services.StakeService()
 
 
@@ -16,14 +16,9 @@ def test_stake_find(kafka_conn, reset_db, seed_stake):
     THEN it should return 1 stake
     """
 
-    global global_stake
-    global global_participant
-
     stakes = stake_service.find()
     assert stakes.total == 1
     assert len(stakes.items) == 1
-    global_stake = stakes.items[0]
-    global_participant = stakes.items[0].participant
 
 
 def test_stake_find_by_uuid(kafka_conn):
@@ -32,12 +27,12 @@ def test_stake_find_by_uuid(kafka_conn):
     WHEN the find method is called with uuid
     THEN it should return 1 stake
     """
-    global global_stake
-    stakes = stake_service.find(uuid=global_stake.uuid)
+
+    stakes = stake_service.find(uuid=pytest.stake.uuid)
     assert stakes.total == 1
     assert len(stakes.items) == 1
     stake = stakes.items[0]
-    assert stake.uuid == global_stake.uuid
+    assert stake.uuid == pytest.stake.uuid
 
 
 def test_stake_find_by_participant_uuid(kafka_conn):
@@ -47,14 +42,11 @@ def test_stake_find_by_participant_uuid(kafka_conn):
     THEN it should return 1 stake
     """
 
-    global global_stake
-    global global_participant
-
-    stakes = stake_service.find(participant_uuid=global_participant.uuid)
+    stakes = stake_service.find(participant_uuid=pytest.participant.uuid)
     assert stakes.total == 1
     assert len(stakes.items) == 1
     stake = stakes.items[0]
-    assert stake.participant.uuid == global_participant.uuid
+    assert stake.participant.uuid == pytest.participant.uuid
 
 
 def test_stake_find_expand_participant(kafka_conn):
@@ -64,14 +56,11 @@ def test_stake_find_expand_participant(kafka_conn):
     THEN it should return 1 stake
     """
 
-    global global_stake
-    global global_participant
-
     stakes = stake_service.find(expand=['participant'])
     assert stakes.total == 1
     assert len(stakes.items) == 1
     stake = stakes.items[0]
-    assert stake.participant.uuid == global_participant.uuid
+    assert stake.participant.uuid == pytest.participant.uuid
 
 
 def test_stake_find_w_pagination(kafka_conn, seed_stake):
@@ -163,44 +152,31 @@ def test_stake_create(kafka_conn, reset_db, seed_participant):
     WHEN the create method is called
     THEN it should return 1 stake and add 1 stake instance into the database
     """
-    global global_participant
-    global global_stake
-
-    participants = services.ParticipantService().find()
-    global_participant = participants.items[0]
-
-    stake = stake_service.create(participant=global_participant, amount=5.0)
+    stake = stake_service.create(participant=pytest.participant, amount=5.0)
     assert stake.uuid is not None
     assert stake.participant is not None
 
 
-def test_stake_create_dup_participant(kafka_conn, get_member_uuid):
+def test_stake_create_dup_participant(kafka_conn):
     """
     GIVEN 1 stake instance in the database
     WHEN the create method is called with duplicate participant
     THEN it should return 0 stake and add 0 stake instance into the database and ManualException with code 500
     """
-    global global_participant
 
     try:
-        _ = stake_service.create(participant=global_participant, amount=5.0)
+        _ = stake_service.create(participant=pytest.participant, amount=5.0)
     except ManualException as ex:
         assert ex.code == 500
 
 
-def test_stake_create_w_participant_uuid(kafka_conn, reset_db, seed_participant):
+def test_stake_create_w_participant_uuid(kafka_conn, reset_db, seed_wager, seed_party, seed_participant):
     """
     GIVEN 0 stake instance in the database
     WHEN the create method is called with participant_uuid
     THEN it should return 1 stake and add 1 stake instance into the database
     """
-    global global_participant
-    global global_stake
-
-    participants = services.ParticipantService().find()
-    global_participant = participants.items[0]
-
-    stake = stake_service.create(participant_uuid=global_participant.uuid, amount=5.0)
+    stake = stake_service.create(participant_uuid=pytest.participant.uuid, amount=5.0)
     assert stake.uuid is not None
 
 
@@ -210,7 +186,6 @@ def test_stake_create_wo_participant(kafka_conn):
     WHEN the create method is called without participant
     THEN it should return 0 stake and add 0 stake instance into the database and ManualException with code 500
     """
-    global global_participant
 
     try:
         _ = stake_service.create(amount=5.0)
@@ -224,26 +199,22 @@ def test_stake_create_w_non_existent_participant_uuid(kafka_conn):
     WHEN the create method is called with non existent participant uuid
     THEN it should return 0 stake and add 0 stake instance into the database and ManualException with code 500
     """
-    global global_participant
+
     try:
         _ = stake_service.create(participant_uuid=generate_uuid(), amount=5.0)
     except ManualException as ex:
         assert ex.code == 500
 
 
-def test_stake_create_w_bad_amount(kafka_conn, reset_db, seed_participant):
+def test_stake_create_w_bad_amount(kafka_conn, reset_db, seed_wager, seed_party, seed_participant):
     """
     GIVEN 1 stake instance in the database
     WHEN the create method is called with invalid amount
     THEN it should return 0 stake and add 0 stake instance into the database and ManualException with code 500
     """
-    global global_participant
-
-    participants = services.ParticipantService().find()
-    global_participant = participants.items[0]
 
     try:
-        _ = stake_service.create(participant=global_participant, amount='five')
+        _ = stake_service.create(participant=pytest.participant, amount='five')
     except ManualException as ex:
         assert ex.code == 500
 
@@ -254,9 +225,8 @@ def test_stake_create_wo_amount(kafka_conn):
     WHEN the create method is called without amount
     THEN it should return 1 stake and add 1 stake instance into the database
     """
-    global global_participant
 
-    stake = stake_service.create(participant=global_participant)
+    stake = stake_service.create(participant=pytest.participant)
     assert stake.amount == 0.0
 
 
@@ -266,11 +236,10 @@ def test_stake_create_w_bad_field(kafka_conn, reset_db, seed_participant):
     WHEN the create method is called with a non existent field
     THEN it should return 0 stake and add 0 stake instance into the database and ManualException with code 500
     """
-    global global_participant
 
     participants = services.ParticipantService().find()
-    global_participant = participants.items[0]
+    participant = participants.items[0]
     try:
-        _ = stake_service.create(participant=global_participant, amount=5.0, junk='junk')
+        _ = stake_service.create(participant=participant, amount=5.0, junk='junk')
     except ManualException as ex:
         assert ex.code == 500
