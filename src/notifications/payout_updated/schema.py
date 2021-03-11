591be4640bed40476a65db8ffaf70538dae74d56
Dallan Bhatti <dallan.bhatti@techtapir.com>
2021-03-11 12:25:28 -0800

    fix issues with decorators tests

--- src/notifications/payout_updated/schema.py
+++ src/notifications/payout_updated/schema.py
@@ -1,9 +1,6 @@
 from marshmallow import Schema, pre_dump
 from webargs import fields
 
-from src import services
-from src.common import ManualException
-
 
 class PayoutUpdatedSchema(Schema):
     uuid = fields.UUID(attribute='wager.uuid')
@@ -12,8 +9,5 @@ class PayoutUpdatedSchema(Schema):
     @pre_dump
     def prepare(self, data, **kwargs):
         wager = data['wager']
-        contests = services.ContestService().find(wager_uuid=wager.uuid)
-        if contests.total != 1:
-            raise ManualException(err=f"issue with contest associated with wager uuid: {str(wager.uuid)}")
-        data['contest'] = contests.items[0]
+        data['contest'] = wager.contest
         return data
